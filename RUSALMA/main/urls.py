42388dd48570a813af21b821d7b3d767cee91ca0
Stefan Vasilenko <vasilencko.stef2013@yandex.ru>
2020-10-05 10:32:26 +0300

    add web_dev mobile version

--- RUSALMA/main/urls.py
+++ RUSALMA/main/urls.py
@@ -4,10 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
     path('blog/post/<int:pk>', views.post, name='post'),
     path('blog/', views.blog, name='blog'),
+
     path('about/', views.about, name='about'),
-    path('chat-bots/', views.chat_bots, name='chat-bots'),
     path('portfolio/', views.portfolio, name='portfolio'),
     path('contacts/', views.contacts, name='contacts'),
 
--- RUSALMA/main/views.py
+++ RUSALMA/main/views.py
@@ -7,6 +7,7 @@ from .models import *
 
 # Create your views here.
 
+
 def index(request):
     return render(request, 'main/index.html')
 
