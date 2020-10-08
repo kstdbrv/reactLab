550252c56813b77d90cb49e77f40fee8e514b91b
Stefan Vasilenko <vasilencko.stef2013@yandex.ru>
2020-10-08 10:58:55 +0300

    fix about page

--- RUSALMA/static/js/slider.js
+++ RUSALMA/static/js/slider.js
@@ -1,9 +1,25 @@
-jQuery(document).ready(function($){
-    $('.center').slick({
-        centerMode: true,
-        centerPadding: '0px',
-        slidesToShow: 3,
-        nextArrow: '<button class="sl-btn next">ᐳ</button>',
-        prevArrow: '<button class="sl-btn prev">ᐸ</button>',
+if (document.documentElement.clientWidth < '480') {
+    jQuery(document).ready(function($){
+        $('.center').slick({
+            centerMode: true,
+            centerPadding: '0px',
+            slidesToShow: 1,
+            nextArrow: '<button class="sl-btn next">ᐳ</button>',
+            prevArrow: '<button class="sl-btn prev">ᐸ</button>',
+          });
       });
-  });
+    
+} else {
+    jQuery(document).ready(function($){
+        $('.center').slick({
+            centerMode: true,
+            centerPadding: '0px',
+            slidesToShow: 3,
+            nextArrow: '<button class="sl-btn next">ᐳ</button>',
+            prevArrow: '<button class="sl-btn prev">ᐸ</button>',
+          });
+      });
+}
+
+
+
