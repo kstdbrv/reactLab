ba5dc0ca03ffa22bcb0cc88b88491fb3e6cd86f0
Stefan Vasilenko <vasilencko.stef2013@yandex.ru>
2020-10-14 19:48:53 +0300

    del smart-selects

--- RUSALMA/RUSALMA/urls.py
+++ RUSALMA/RUSALMA/urls.py
@@ -22,7 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('chaining/', include('smart_selects.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
